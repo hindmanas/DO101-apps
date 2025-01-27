f3bc724bf9081336ad5b168657ed72061f5020d8
hindmanas <manaspandya4039@gmail.com>
2025-01-27 14:48:22 +0530

    updated scale branch

--- php-scale/index.php
+++ php-scale/index.php
@@ -1,3 +1,3 @@
 <?php
-  print "I am running on host -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
+  print "I am running on host for redahat 2nd course -> " . gethostname() ." (" . $_SERVER['SERVER_ADDR'] . ")" . "\n";
 ?>
